4076d8a56c336f11c0d8c8649023d9e4fde99f22
DobekMarcin <reinawen@gmail.com>
2024-03-07 14:30:59 +0100

    Rozdzielnik płacowy - rozdzielnik KORG/PRACOWNIK v6 Poprawki po pierwszym księgowaniu

--- src/main/java/md/enovaImport/controllers/BookKeepingPatternPositionWindowController.java
+++ src/main/java/md/enovaImport/controllers/BookKeepingPatternPositionWindowController.java
@@ -2,7 +2,6 @@ package md.enovaImport.controllers;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
